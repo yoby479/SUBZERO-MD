d78687d98fb50ebb8f3815a640596b38050381ab
yoby479 <yobimnare@gmail.com>
2025-09-10 03:37:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;PRl0nQYK#EPM6rxH0qgrZPFl42wT_L50siOQlkmOTDOvAVxitglM", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254798457039", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
