e8793224b12d8d0cdcfcb66005b97abc4421ec97
∆•π•TOXIC TECH PLANS😎•π•∆ <yobimnare@gmail.com>
2026-02-14 11:14:10 +0300

    Update settings.js for Jdjd

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;PRl0nQYK#EPM6rxH0qgrZPFl42wT_L50siOQlkmOTDOvAVxitglM", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "254798457039", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~8XxtrR",
+  OWNER_NUMBER: "+254 737 014543", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
